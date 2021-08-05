ef21d220bcd979de7e382091137c74fc9a65d2c4
weirdyang <wy4786@gmail.com>
2021-08-05 16:44:32 +0800

    adding production settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  userApi: 'https://sgsew-backend.herokuapp.com',
+  journalApi: 'https://sgsew-backend.herokuapp.com',
+  authApi: 'https://sgsew-backend.herokuapp.com',
+  productApi: 'https://sgsew-backend.herokuapp.com'
 };
