ceff888111a0ec9b88cd88a8d1f6d8178659a5cc
weirdyang <wy4786@gmail.com>
2021-08-19 02:05:05 +0800

    feat(user-list): add admin guard for users module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminGuard } from './services/guards/admin.guard';
 
-const routes: Routes = [{ path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) }, { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }, { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }];
+const routes: Routes = [
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('./users/users.module').then(m => m.UsersModule),
+    canLoad: [AdminGuard]
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
